0bd52f16e3a129009666d10642bf3d52255ecb57
jinlee0310 <onenmore.jin@gmail.com>
2022-03-29 00:47:16 +0900

    feat:폴더구조 잡기

---
+++ src/components/findRoadmap/RoadmapCard.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function RoadmapCard() {
+  return (
+    <div>
+      <h2>로드맵 이름</h2>
+      <button>화살표</button>
+      <div>로드맵 상세설명</div>
+      {/* <image src={} alt="thumbnail"/> */}
+    </div>
+  );
+}
+
+export default RoadmapCard;
