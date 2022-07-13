bcf2dfa2f98f87621b9c7cd00d70265204c79cf4
jinlee0310 <onenmore.jin@gmail.com>
2022-07-13 23:37:36 +0900

    ncs 데이터 추가

--- src/containers/homePage/Roadmap.tsx
+++ src/containers/homePage/Roadmap.tsx
@@ -1,58 +1,31 @@
-import RoadmapCard from '@/components/findRoadmap/RoadmapCard';
+import {
+  primaryData,
+  quaternaryData,
+  secondaryData,
+  tertiaryData,
+} from '@/asset/roadmapData';
 import TitleNSelect from '@/components/roadmap/TitleNSelect';
-import Link from 'next/link';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-
-const primaryData = [
-  '사업관리',
-  '기획사무',
-  '금융',
-  '평생교육',
-  '보건',
-  '사회복지',
-  '문화·예술',
-  '자동차운전·운송',
-  '영업',
-  '경비',
-  '이·미용',
-  '식음료조리·서비스',
-  '건설공사관리',
-  '기계설계',
-  '금속재료',
-  '화학물질·화학공정품질관리',
-  '섬유제조',
-  '전기',
-  '정보통신',
-  '식품가공',
-  '인쇄·출판',
-  '산업환경',
-];
-
-const sample = require('@/sampleResponse/sample.json');
-
-const classificationList = ['대분류', '중분류', '소분류', '세분류'];
-
-const secondaryData: { [key: string]: string[] } = {
-  정보통신: ['정보기술', '통신기술', '방송기술'],
-};
-const tertiaryData: { [key: string]: string[] } = {
-  정보기술: ['정보기술전략/ 계획', '정보기술개발', '정보기술관리', '정보보호'],
-};
-const quaternaryData: { [key: string]: string[] } = {
-  정보기술개발: [
-    '정보기술전략/ 계획',
-    '정보기술 개발',
-    '정보기술 관리',
-    '정보보호',
-  ],
-};
 
 function Roadmap() {
   const [primary, setPrimary] = useState('');
   const [secondary, setSecondary] = useState('');
   const [tertiary, setTertiary] = useState('');
   const [quaternary, setQuaternary] = useState('');
+
+  useEffect(() => {
+    setSecondary('');
+    setTertiary('');
+    setQuaternary('');
+  }, [primary]);
+  useEffect(() => {
+    setTertiary('');
+    setQuaternary('');
+  }, [secondary]);
+  useEffect(() => {
+    setQuaternary('');
+  }, [tertiary]);
 
   return (
     <Container>
